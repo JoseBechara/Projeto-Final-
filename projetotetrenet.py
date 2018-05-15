662745df41c3883a764d50b345708f416b5d68d3
JoseBechara <38349966+JoseBechara@users.noreply.github.com>
2018-05-15 16:51:15 -0300

    Add files via upload

--- projetotetrenet.py
+++ projetotetrenet.py
@@ -5,6 +5,7 @@ Created on Fri May  4 08:45:41 2018
 @author: luiza
 """
 
+
 import pygame
 from random import randrange
 import sys
@@ -23,14 +24,15 @@ altura=600
 
 class Quadrado(pygame.sprite.Sprite):
   
-    def _init_(self, arquivo_imagem, pos_x, pos_y):
-        pygame.sprite.Sprite._init_(self)
+    def __init__(self, arquivo_imagem, pos_x, pos_y):
+        pygame.sprite.Sprite.__init__(self)
+#        self.image = pygame.image.load(arquivo_imagem)
+#        self.image = pygame.Surface((100,100))
+#        self.image.fill(azul)
         self.image = pygame.image.load(arquivo_imagem)
-        self.image = pygame.Surface((100,100))
-        self.image.fill(azul)
         self.rect = self.image.get_rect()
         self.rect.x = pos_x
-        self.rect.y = pos_y        
+        self.rect.y = pos_y  
        
     
     def move(self):
@@ -44,14 +46,15 @@ class Quadrado(pygame.sprite.Sprite):
 #        if self.rect.y<600:
 #            if keys[pygame.K_DOWN]:
 #                self.rect.y +=30
-            
+          
 
         
+              
         
 class Chao(pygame.sprite.Sprite):
   
-    def _init_(self, pos_x, pos_y):
-        pygame.sprite.Sprite._init_(self)
+    def __init__(self, pos_x, pos_y):
+        pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((700,2))
         self.image.fill(preto)
         self.rect = self.image.get_rect()
@@ -61,7 +64,7 @@ class Chao(pygame.sprite.Sprite):
     
 
 fundo=pygame.display.set_mode((largura,altura))
-pygame.display.set_caption("tetrinet")
+pygame.display.set_caption("Tetrinet")
 
 
 quadrado = Quadrado('Peca3.png', 200, 0)
@@ -77,6 +80,7 @@ chao_grupo.add(chao)
 
 clock = pygame.time.Clock()
 jogo=True
+fim_do_jogo=False
 
 
 vel_y = 20
@@ -89,50 +93,59 @@ while jogo:
         if event.type == pygame.QUIT:
             jogo = False
             
-    colisao = pygame.sprite.spritecollide(quadrado, chao_grupo, False)
-    for e in colisao:
-        vel_y=0
-        for i in pecas_grupo:
-            pecas_paradas_grupo.add(i)
-        quadrado.rect.y=altura-100    
+    for e in pecas_grupo:
+        if e.rect.y==0 and vel_y==0:
+           fim_do_jogo=True     
+           
+    if not fim_do_jogo:       
+            
+       colisao = pygame.sprite.spritecollide(quadrado, chao_grupo, False)
+       for e in colisao:
+           vel_y=0
+           for i in pecas_grupo:
+               pecas_paradas_grupo.add(i)
+           quadrado.rect.y=altura-100    
         
         
          
 
    
-    colisao = pygame.sprite.spritecollide(quadrado, pecas_paradas_grupo, False)
-    for e in colisao:
-        vel_y = 0
-        for p in pecas_grupo:
-            pecas_paradas_grupo.add(p)
+       colisao = pygame.sprite.spritecollide(quadrado, pecas_paradas_grupo, False)
+       for e in colisao:
+           vel_y = 0
+           for p in pecas_grupo:
+               pecas_paradas_grupo.add(p)
             
             
         
-    if vel_y == 0:
-        aleatorio = randrange(1,7)
-        if aleatorio == 1:
-            quadrado = Quadrado('Peca3.png', 200, 0)
-            pecas_grupo.add(quadrado)
-        if aleatorio == 2:
-            pass
-        if aleatorio == 3:
-            pass
-        if aleatorio == 4:
-            pass
-        if aleatorio == 5:
-            pass
-        if aleatorio == 6:
-            pass
-        if aleatorio == 7:
-            pass
-        vel_y = 20
+       if vel_y == 0:
+           aleatorio = randrange(1,7)
+           if aleatorio == 1:
+               quadrado = Quadrado('Peca3.png', 200, 0)
+               pecas_grupo.add(quadrado)
+           if aleatorio == 2:
+               pass
+           if aleatorio == 3:
+               pass
+           if aleatorio == 4:
+               pass
+           if aleatorio == 5:
+               pass
+           if aleatorio == 6:
+               pass
+           if aleatorio == 7:
+               pass
+           vel_y = 20
         
         
-      
+#    for e in pecas_grupo:
+#        if e.rect.y==0 
+#            pygame.quit() 
+       
         
     
 
-    quadrado.rect.y += vel_y
+       quadrado.rect.y += vel_y
     
 
         
@@ -140,11 +153,11 @@ while jogo:
             
     
     
-    fundo.fill(preto)
-    quadrado.move() 
-    pecas_grupo.draw(fundo)
+       fundo.fill(preto)
+       quadrado.move() 
+       pecas_grupo.draw(fundo)
     
-    pygame.display.update()
+       pygame.display.update()
 
     
     
